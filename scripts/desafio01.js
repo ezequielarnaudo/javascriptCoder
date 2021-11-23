98801e4b7b52076d70084eea9655ec39a453566c
ezequielarnaudo <59714276+ezequielarnaudo@users.noreply.github.com>
2021-11-23 16:45:40 -0300

    .

--- scripts/desafio01.js
+++ scripts/desafio01.js
@@ -12,6 +12,14 @@
 // Código de aquí en adelante
 
 let nombreUsuario = prompt("Ingrese su nombre completo");
+
 alert("Buenas tardes: " + nombreUsuario);
+
 let edadUsuario = prompt("Ingrese su edad");
+
 alert("Buenas tardes: " + nombreUsuario + " su edad es: " + edadUsuario);
+
+let nacimiento = 2021 - edadUsuario;
+
+alert("Buenas tardes: " + nombreUsuario + " tu edad es " + edadUsuario + " y tu año de nacimiento es: " + nacimiento);
+
