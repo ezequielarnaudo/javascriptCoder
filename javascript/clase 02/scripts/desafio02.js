fb745481a0d6eb54fce683ba7cd1c6cb8820b5a2
ezequielarnaudo <59714276+ezequielarnaudo@users.noreply.github.com>
2021-11-25 19:20:20 -0300

    Add files via upload

---
+++ javascript/clase 02/scripts/desafio02.js
@@ -0,0 +1,25 @@
+/**
+ * @challenge: CREAR UN ALGORITMO JS SIMPLE
+ * 
+ * @version: versión: 1.0.0
+ * @author: Ezequiel Arnaudo.
+ * @fecha: 23/11/2021
+ *
+ * History:
+ *  - v1.0.0 – Primera entrega
+ */
+
+// Código de aquí en adelante
+
+let nombreUsuario = prompt("Ingrese su nombre completo");
+
+alert("Buenas tardes: " + nombreUsuario);
+
+let edadUsuario = prompt("Ingrese su edad");
+
+alert("Buenas tardes: " + nombreUsuario + " su edad es: " + edadUsuario);
+
+let nacimiento = 2021 - edadUsuario;
+
+alert("Buenas tardes: " + nombreUsuario + " tu edad es " + edadUsuario + " y tu año de nacimiento es: " + nacimiento);
+
