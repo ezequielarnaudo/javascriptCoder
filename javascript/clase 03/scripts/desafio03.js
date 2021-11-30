91d11b090f57dce09be08b91757fa5786fb4e54f
ezequielarnaudo <59714276+ezequielarnaudo@users.noreply.github.com>
2021-11-30 20:29:34 -0300

    .

--- javascript/clase 03/scripts/desafio03.js
+++ javascript/clase 03/scripts/desafio03.js
@@ -6,31 +6,15 @@
  * @fecha: 23/11/2021
  *
  * History:
- *  - v1.0.0 – Primera entrega
+ *  - v1.0.3 – Primera entrega
  */
 
 // Código de aquí en adelante
 
  
-/* let numero = parseInt(prompt("INGRESE UN NUMERO ENTRE 0 Y 100"));
-
- if(numero > 100){
-     alert("El numero ingresado es " + numero + " y no cumple con la consigna" );
- }else{
-     alert("El numero ingresado es " + numero);
- }
- 
- let num1 = parseInt(prompt("INGRESE UN NUMERO"));
- if(isNaN(num1)){
-	alert(" Usted NO ingreso un numero");
-    
- }else{
-	alert("Usted ingreso correctamente el numero " + num1);
- }
-*/
 
 
-/*const password = prompt("Establezca una contraseña");
+const password = prompt("Establezca una contraseña");
 
 while(true) {
 
@@ -49,11 +33,4 @@ while(true) {
     break;
 
   }
-}
-*/
-for (let i = 0; i <= 20; i++);{
-    
-    let nombreTurno = prompt("Ingrese su nombre");
-
-    alert("Hola" + nombreTurno + "su turno es el numero" + i);
 }
