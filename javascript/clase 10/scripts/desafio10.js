615cdd46603a2ce9d96a5a76120fa3fd74abdad7
ezequielarnaudo <59714276+ezequielarnaudo@users.noreply.github.com>
2022-01-06 18:32:02 -0300

    cambios

--- javascript/clase 10/scripts/desafio10.js
+++ javascript/clase 10/scripts/desafio10.js
@@ -14,17 +14,17 @@
  *  - v1.0.6 – Sexta entrega
  *  - v1.0.8 – Octava entrega
  *  - v1.0.9 – Novena entrega
+ *  - v1.0.10 - Décima entrega
  */
 
-// Código de aquí en adelante
+
 
 //llamada a las funciones para cancelar o guardar info al hacer click en uno u otro btn.
-
 let botonCancelar = document.getElementById("btnCancelar");
 botonCancelar.addEventListener("click", Cancelar);
 
 let botonAceptar = document.getElementById("btnAceptar");
-botonAceptar.addEventListener("click", Guardar);
+botonAceptar.addEventListener("click", guardar);
 
 
 
@@ -42,50 +42,66 @@ function Cancelar()
 
 
 
-// esta funcion guarda la info de los profesionales que se dan de alta en el sistema y los ordena en una tabla
+let datoStorage;
+let listado;
 
-function Guardar()
-{
-    let nombre = document.getElementById("nombreDoc").value;
-    let apellido = document.getElementById("apellidoDoc").value;
-    let matricula = document.getElementById("matriculaDoc").value;
-    let especialidad = document.getElementById("especialidadDoc").value;
-    let cuerpoTabla = document.getElementById("cuerpoTabla");
-    let fila = document.createElement("tr");
-    fila.innerHTML="<td>" + nombre + " " + apellido + "</td>";
-    fila.innerHTML+="<td>" + matricula + "</td>";
-    fila.innerHTML+="<td>" + especialidad + "</td>";
-    cuerpoTabla.appendChild(fila);
-    Cancelar();
+function leerLocalStorage(){
+
+     datoStorage = localStorage.getItem('datos'); //lee local storage y lo guarda en una variable
     
-        localStorage.setItem ('nombre', nombre);
-        localStorage.setItem ('apellido', apellido);
-        localStorage.setItem ('especialidad', especialidad);
+       if (datoStorage == null) //si la variable previamente seteada es null
+       {
+           localStorage.setItem('datos', '[]') //almacena un string que representa un array vacio
+           
+           datoStorage = localStorage.getItem('datos'); //obtiene de vuelta el localstorage 
+                                                        //y lo vuelve a guardar en la variable
+       } 
+
+    listado = JSON.parse(datoStorage); //convierte la cariable los valores de 
+                                       //local storage y los convierte en OBJETOS
+
+    //obtiene el DOM del cuerpo de la tabla
+    let cuerpoTabla = document.getElementById("cuerpoTabla");
+    //limpia el cuerpo de la tabla
+    cuerpoTabla.innerHTML="";
+
+    //FOR: declara contador en cero, si el contador es menor al ARRAY DE OBJETOS, incrementa contador
+    for (let contador = 0; contador < listado.length; contador++) {
+         
+        let fila = document.createElement("tr");//crea un DOM TR (fila de tabla)
+        fila.innerHTML="<td>" + listado[contador].nombre + " " +listado[contador].apellido +  "</td>";// setea el html de la fila con una celda adentro y accede a la posicion del arreglo o array o matriz
+        fila.innerHTML+="<td>" + listado[contador].matricula + "</td>";
+        fila.innerHTML+="<td>" + listado[contador].especialidad + "</td>";
+        cuerpoTabla.appendChild(fila); // pega la fila que armamos anteriormente
+    }
 }
 
+//guarda los datos ingresados
+function guardar () {
 
-    const guardarValores = (nombre, apellido, especialidad) => {
+    let nombre = document.getElementById("nombreDoc").value;
+    let apellido = document.getElementById("apellidoDoc").value;
+    let matricula = parseInt(document.getElementById("matriculaDoc").value); 
+    let especialidad = document.getElementById("especialidadDoc").value;
 
-        return localStorage.getItem(nombre, apellido, especialidad);
-    
+    datoStorage = localStorage.getItem('datos');
+    listado = JSON.parse(datoStorage);
+    let medico = new personaMedico(nombre, apellido, matricula, especialidad);
+    listado.push(medico);
+    let listadoJson = JSON.stringify(listado);
+    localStorage.setItem('datos', listadoJson);
+    Cancelar();
+    leerLocalStorage();
+}
+
+class personaMedico {
+
+    constructor(nombre, apellido, especialidad, matricula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.matricula = matricula;
     }
+}
 
-let ejemplo = guardarValores();
-
-
-
-
-/*
-localStorage.setItem ('nombre', 'ezequiel');
-localStorage.setItem ('apellido', apellido);
-*/
-
-
-
-
-
-
-
-
-
-
+leerLocalStorage();
